1f0e97c9d9410fd982e6ec7b24669e8e2cf30e10
hovhannisyan-hovhannes <hovhannisyan19081987@gmail.com>
2024-06-08 11:13:34 +0400

    myfirstapp

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+
+
+class App extends React.Component{
+  render(){
+    return <header className="App-header">
+      <div>
+        <h1>Բարև ես Հովհաննեսն եմ</h1>
+      </div>
+    </header> 
+  }
+}
+export default App
